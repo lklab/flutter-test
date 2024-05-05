953cbc87c2065ebe5b9017e60eb24a0328222d84
lklab <alexeibatic@gmail.com>
2024-05-05 20:55:22 +0900

    캐릭터 카드 앱 2

--- character_card/lib/main.dart
+++ character_card/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BBANTO',
       home: Grade(),
     );
@@ -30,22 +31,34 @@ class Grade extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/fire-3352_256.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            const Text(
               'NAME',
               style: TextStyle(
                 color: Colors.white,
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10.0,
             ),
-            Text(
+            const Text(
               'BBANTO',
               style: TextStyle(
                 color: Colors.white,
@@ -54,6 +67,83 @@ class Grade extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              'BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              '14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'using lightsaber',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'face hero tattoo',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'fire flames',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: const AssetImage('assets/Cruiser 3.png'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            )
           ],
         ),
       ),

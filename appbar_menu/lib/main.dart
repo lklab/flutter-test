f79950d5473031f12b193760d43e68f3daae1740
lklab <alexeibatic@gmail.com>
2024-05-16 23:56:34 +0900

    Drawer 메뉴 추가

--- appbar_menu/lib/main.dart
+++ appbar_menu/lib/main.dart
@@ -31,12 +31,6 @@ class MyPage extends StatelessWidget {
         foregroundColor: Colors.white,
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is clicked');
-          },
-        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.shopping_cart),
@@ -51,6 +45,70 @@ class MyPage extends StatelessWidget {
             },
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/Cruiser3.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/Destroyer1.png'),
+                  backgroundColor: Colors.white,
+                ),
+              ],
+              accountName: const Text('BBANTO'),
+              accountEmail: const Text('bbanto@bbanto.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Home'),
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Setting'),
+              onTap: () {
+                print('Setting is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: const Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }

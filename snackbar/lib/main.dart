43e8f1f41e920b60de42aab9794108f910b75407
lklab <alexeibatic@gmail.com>
2024-05-25 22:02:41 +0900

    스낵바 및 토스트

--- snackbar/lib/main.dart
+++ snackbar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,28 +27,74 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Snack Bar'),
+        title: const Text('Snack Bar & Toast'),
         backgroundColor: Colors.red,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Hello'),
-            ));
-          },
-          style: ButtonStyle(
-            textStyle: MaterialStateProperty.all(const TextStyle(
-              fontSize: 15,
-            )),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            backgroundColor: MaterialStateProperty.all(Colors.blue),
-          ),
-          child: const Text('Show me'),
-        ),
-      ),
+      body: const MySnackBar(),
     );
   }
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text(
+                  'Hello',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                backgroundColor: Colors.teal,
+                duration: Duration(
+                  milliseconds: 1000,
+                ),
+              ));
+            },
+            style: ButtonStyle(
+              textStyle: MaterialStateProperty.all(const TextStyle(
+                fontSize: 15,
+              )),
+              foregroundColor: MaterialStateProperty.all(Colors.white),
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+            child: const Text('Show SnackBar'),
+          ),
+          ElevatedButton(
+            onPressed: flutterToast,
+            style: ButtonStyle(
+              textStyle: MaterialStateProperty.all(const TextStyle(
+                fontSize: 15,
+              )),
+              foregroundColor: MaterialStateProperty.all(Colors.white),
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+            child: const Text('Show Toast'),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+    msg: 'Hello12',
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: Colors.redAccent,
+    fontSize: 20.0,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT,
+  );
 }

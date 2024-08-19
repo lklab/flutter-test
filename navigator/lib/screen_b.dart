83cef1e055be339d97d91bdbda45d1e83cb1f56b
lklab <alexeibatic@gmail.com>
2024-08-19 21:38:43 +0900

    lifecycle method 테스트

--- navigator/lib/screen_b.dart
+++ navigator/lib/screen_b.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ScreenB extends StatelessWidget {
+class ScreenB extends StatefulWidget {
   const ScreenB({super.key});
 
   @override
+  State<ScreenB> createState() => _ScreenBState();
+}
+
+class _ScreenBState extends State<ScreenB> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState is called');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose is called');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build is called');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Screen B'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text(
-          'Screen B',
-          style: TextStyle(
-            fontSize: 24.0,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text(
+            'Go to the Screen A',
+            style: TextStyle(
+              fontSize: 24.0,
+            ),
           ),
-        )
+        ),
       ),
     );
   }

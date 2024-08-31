d6cf9d6112d337678c18fec0d3fdd0636e6f3487
lklab <alexeibatic@gmail.com>
2024-08-31 13:28:41 +0900

    json 추가

--- weather_app/lib/screens/loading.dart
+++ weather_app/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -9,14 +11,20 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text('Get my location'),
         ),
       ),
@@ -30,8 +38,27 @@ class _LoadingState extends State<Loading> {
       accuracy: LocationAccuracy.high,
       distanceFilter: 100,
     );
-    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
 
-    print(position);
+    try {
+      Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+      print(position);
+    }
+    catch(e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async {
+    Uri uri = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+    http.Response response = await http.get(uri);
+
+    if(response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData);
+      var description = myJson['weather'][0]['description'];
+      var wind = myJson['wind']['speed'];
+      var id = myJson['id'];
+      print('description: $description\nwind: $wind\nid: $id');
+    }
   }
 }

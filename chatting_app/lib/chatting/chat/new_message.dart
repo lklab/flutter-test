9d61e6cbf5881f832a0b0686c8c5a9b40e4ae1ff
lklab <alexeibatic@gmail.com>
2024-10-20 20:44:16 +0900

    채팅 메시지 보내는 기능 추가

---
+++ chatting_app/lib/chatting/chat/new_message.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _userEnterMessage = '';
+  final _controller = TextEditingController();
+
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    FirebaseFirestore.instance.collection('chat').add({
+      'text' : _userEnterMessage,
+      'time' : Timestamp.now(),
+    });
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Send a message...',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _userEnterMessage = value;
+                });
+              },
+              controller: _controller,
+            ),
+          ),
+          IconButton(
+            onPressed: _userEnterMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send),
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- chatting_app/lib/screens/chat_screen.dart
+++ chatting_app/lib/screens/chat_screen.dart
@@ -1,6 +1,8 @@
+import 'package:chatting_app/chatting/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:chatting_app/chatting/chat/message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -51,32 +53,15 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('chats/pw4k8xBHPSCnF36EA0E4/message').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  docs[index]['text'],
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-              );
-            },
-          );
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }

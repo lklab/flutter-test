beb0f1cb27eb8dea196a18c8591047080031ce79
lklab <alexeibatic@gmail.com>
2024-10-21 13:56:07 +0900

    이미지 추가 UI 디자인

---
+++ chatting_app/lib/add_image/add_image.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AddImage extends StatefulWidget {
+  const AddImage({super.key});
+
+  @override
+  State<AddImage> createState() => _AddImageState();
+}
+
+class _AddImageState extends State<AddImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 300,
+      padding: EdgeInsets.only(top: 10),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.blue,
+          ),
+          SizedBox(height: 10,),
+          OutlinedButton.icon(
+            onPressed: () {
+
+            },
+            icon: Icon(Icons.image),
+            label: Text('Add icon'),
+          ),
+          SizedBox(height: 80,),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.close),
+            label: Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+}

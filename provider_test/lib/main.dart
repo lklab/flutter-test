08b05df44accb3883015d082c680bffe4dda122a
lklab <alexeibatic@gmail.com>
2024-10-22 12:34:39 +0900

    위젯 구성

--- provider_test/lib/main.dart
+++ provider_test/lib/main.dart
@@ -10,11 +10,197 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: FishOrder(),
+    );
+  }
+}
+
+class FishOrder extends StatelessWidget {
+  const FishOrder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fish Order'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Fish name',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            High(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class High extends StatelessWidget {
+  const High({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'SpicyA is located at high place',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SpicyA(),
+      ],
+    );
+  }
+}
+
+class SpicyA extends StatelessWidget {
+  const SpicyA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Fish number',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Fish size',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Middle(),
+      ],
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  const Middle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'SpicyB is located at middle place',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SpicyB(),
+      ],
+    );
+  }
+}
+
+class SpicyB extends StatelessWidget {
+  const SpicyB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Fish number',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Fish size',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Low(),
+      ],
+    );
+  }
+}
+
+class Low extends StatelessWidget {
+  const Low({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'SpicyC is located at low place',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SpicyC(),
+      ],
+    );
+  }
+}
+
+class SpicyC extends StatelessWidget {
+  const SpicyC({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Fish number',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Fish size',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
 }

3d70055bdbb029ae31a1d292cb9f59a171b3f30c
lklab <alexeibatic@gmail.com>
2024-10-23 15:27:03 +0900

    Update main.dart

--- riverpod_test/lib/main.dart
+++ riverpod_test/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +14,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Riverpod Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TestView(),
+    );
+  }
+}
+
+class TestView extends ConsumerWidget {
+  final counterProvider = StateNotifierProvider((ref) => Counter());
+
+  TestView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+    ref.listen(counterProvider, (prev, next) {
+      print('State changed: $prev, $next');
+    });
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Count: $count',
+            ),
+            TextButton(
+              onPressed: () {
+                ref.watch(counterProvider.notifier).increment();
+              },
+              child: const Text(
+                'Increment',
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+
+  void increment() => state++;
 }
